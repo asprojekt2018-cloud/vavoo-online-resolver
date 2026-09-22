b3e48cfbfa12e38af88bf7a2c39b2f0f5ced4f9c
asprojekt2018-cloud <asprojekt2018@gmail.com>
2026-09-22 23:01:08 +0200

    Update vavoo_resolver_all.py

--- vavoo_resolver_all.py
+++ vavoo_resolver_all.py
@@ -128,8 +128,7 @@ def playlist():
         logo = esc(ch.get("logo") or "")
         lines.append('#EXTINF:-1 tvg-name="%s" tvg-logo="%s" group-title="%s",%s' %
                      (name, logo, group, name))
-        lines.append("http://%s:%d/play/%s" %
-                     (HOST, PORT, urllib.parse.quote(cid, safe="")))
+        
     return ("\n".join(lines) + "\n").encode("utf-8")
 
 class Handler(BaseHTTPRequestHandler):
